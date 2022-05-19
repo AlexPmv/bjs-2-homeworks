1171640f31248028cde5e319d70f8f135689da0b
Alex Palmov <likeitsweet@mail.ru>
2022-05-20 00:00:18 +0300

    3.arrays task solution added

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,11 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
-  return result; // boolean
+  let workArray = Array.from(arguments);
+  let checkValue = workArray[0].join('');
+  return result = workArray.every((arr) => arr.join('') === checkValue);
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  return resultArr = arr.filter((item) => item >= 0 && item % 3 === 0).map((item) => item * 10);
 }
