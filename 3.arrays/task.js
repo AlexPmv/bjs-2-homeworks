185977438fcaa3f8ac181d66b5b8e81ebc74d2aa
Alex Palmov <likeitsweet@mail.ru>
2022-05-20 21:05:05 +0300

    3.arrays task re-edited

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,8 +1,6 @@
 function compareArrays(arr1, arr2) {
-  return arr1.every((item, idx, arr1) => 
-  arr1.length === arr2.length &&
-  arr1.every((item, idx) => item === arr2[idx])
-  );
+  return arr1.length === arr2.length &&
+  arr1.every((item, idx) => item === arr2[idx]);
 }
 
 function advancedFilter(arr) {
