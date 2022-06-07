1d9180eec56909d1ec9daaaa699de98abfea1efe
Alex Palmov (PC) <likeitsweet@mail.ru>
2022-06-07 16:02:29 +0300

    8.decorators task added

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,53 @@
+function add(...args) {
+  return args.reduce((acc, item) => acc += item);
+};
+
 function cachingDecoratorNew(func) {
-  // Ваш код
-}
+  let hash = {};
 
+  return (...args) => {
+    const currentArgs = args.join(',');
+    
+    if (currentArgs in hash) {
+      return `Из кэша: ${hash[currentArgs]}`;
+    } else if (Object.keys(hash).length === 5){
+      hash = Object.fromEntries(Object.entries(hash).splice(1));
+    };
 
-function debounceDecoratorNew(func) {
-  // Ваш код
-}
+    return `Вычисляем: ${hash[currentArgs] = func(...args)}`;
+  };
+};
+
+const addWithCashing = cachingDecoratorNew(add);
+
+const sendSignal = () => console.log("Сигнал отправлен");
+
+function debounceDecoratorNew(func, ms) {
+  let isThrottled = false, timerId;
+
+  return () => {
+    if (isThrottled) {
+      clearTimeout(timerId);
+    } else {
+      func();
+      isThrottled = true;
+    };
+
+    timerId = setTimeout(() => {
+      isThrottled = false;
+      func();
+    }, ms);
+  };
+};
+
+const upgradedSendSignal = debounceDecoratorNew(sendSignal, 2000);
 
 function debounceDecorator2(func) {
-  // Ваш код
-}
+  return () => {
+    func.count === undefined ? func.count = 1 : func.count++;
+    console.log(func.count);
+    func();
+  };
+};
+
+const upgradedSendSignal2 = debounceDecorator2(upgradedSendSignal);
